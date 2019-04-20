0a63cf93941f601381c9bc60a57302e8d629c44f
inzahgi <inzahgi@126.com>
2019-04-20 18:22:48 +0800

    add file

--- leetcode/src/main/java/com/inzahgi/leetcodeExample/onlyTest/meeting/model/TimeWheel.java
+++ leetcode/src/main/java/com/inzahgi/leetcodeExample/onlyTest/meeting/model/TimeWheel.java
@@ -9,6 +9,12 @@ public class TimeWheel {
 
     private int curMin;
 
+    public TimeWheel(int startHour, int startMin){
+        hours = new int[24];
+        mins = new int[60];
+        curHour = startHour;
+        curMin = startMin;
+    }
 
 
 }
