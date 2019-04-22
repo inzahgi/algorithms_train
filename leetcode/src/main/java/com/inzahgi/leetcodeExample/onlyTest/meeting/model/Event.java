3ae096fcfa48b896e95394bb345e1316d20b065c
inzahgi <inzahgi@126.com>
2019-04-22 23:53:19 +0800

    add

---
+++ leetcode/src/main/java/com/inzahgi/leetcodeExample/onlyTest/meeting/model/Event.java
@@ -0,0 +1,27 @@
+package com.inzahgi.leetcodeExample.onlyTest.meeting.model;
+
+public class Event {
+
+    private String Name;
+
+    private long start;
+
+    
+
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public long getStart() {
+        return start;
+    }
+
+    public void setStart(long start) {
+        this.start = start;
+    }
+}
